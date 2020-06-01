a58514b62145e412b9d00d160311264797159d69
Luis-Enrique1 <piowanau@gmail.com>
2020-05-31 22:16:47 -0400

    Updates

---
+++ component---src-pages-404-js-47345483bf9b6f0686c8.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{w2l6:function(n,e,t){"use strict";t.r(e);var a=t("q1tI"),u=t.n(a),l=t("5w8u");e.default=function(){return u.a.createElement(l.a,null,u.a.createElement("h2",null,"404 page not found"))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-47345483bf9b6f0686c8.js.map
